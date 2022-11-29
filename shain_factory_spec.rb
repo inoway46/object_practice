50d3e3a8154fd1747b19c744e8f7e89b7ba7f3f8
inoway46 <inoueyuya416@gmail.com>
2022-11-29 10:52:55 +0900

    Add 各役職のテストを追加

--- shain_factory_spec.rb
+++ shain_factory_spec.rb
@@ -3,7 +3,19 @@ require_relative 'shain_factory'
 describe 'ShainFactory' do
   let(:shain_factory) { ShainFactory.new }
 
-  example 'Tantoオブジェクトを生成しているかどうかをチェックする' do
+  example 'Tantoオブジェクトを生成しているかをチェック' do
     expect(shain_factory.create('Tanto', 100)).to be_an_instance_of Tanto
+  end
+
+  example 'Shuninオブジェクトを生成しているかをチェック' do
+    expect(shain_factory.create('Shunin', 100)).to be_an_instance_of Shunin
+  end
+
+  example 'Buchoオブジェクトを生成しているかをチェック' do
+    expect(shain_factory.create('Bucho', 100)).to be_an_instance_of Bucho
+  end
+
+  example 'Torishimariyakuオブジェクトを生成しているかをチェック' do
+    expect(shain_factory.create('Torishimariyaku', 100)).to be_an_instance_of Torishimariyaku
   end
 end
